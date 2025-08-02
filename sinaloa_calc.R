5316ecef9796e20b8f5bbcfadffa36e93ac8ece2
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-08-02 12:49:21 -0700

    employment all mats

--- sinaloa_calc.R
+++ sinaloa_calc.R
@@ -5,8 +5,22 @@ MIP <- "data/mip_sinaloa.tsv"
 EMPLOYMENT <- "data/enoe_sinaloa.tsv"
 
 mip <- read_tsv(MIP)
-employment <- read_tsv(EMPLOYMENT)
+employment <- read_tsv(EMPLOYMENT) |>
+  filter(!is.na(sector))
 
 sinaloa <- get_ZAB_LG_fx_Madds(mip)
 
-attach(sinaloa)
+get_T <- function(L, x, E) {
+  e <- E / x
+  Tm <- diag(e) %*% L
+  mask <- is.nan(Tm) | is.infinite(Tm) | is.na(Tm)
+  Tm[mask] <- 0
+  Tm
+}
+
+etype <- c("empleos", "formales", "informales")
+
+Tsin <- employment[etype] |>
+  map(get_T, L = sinaloa$L, x = sinaloa$x)
+
+Tsin <- set_names(Tsin, etype)
