7ecfe93f539be9352154c6e86ddd0d8fc8c7eb00
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-09-19 16:31:43 -0700

    dir, indir, induc

--- analysis_2025-09-22.R
+++ analysis_2025-09-22.R
@@ -22,3 +22,27 @@ percents_effects <- results |>
   set_names(str_c("P_", DIR_INDIR)
 )
 
+
+# cons <- percents_effects |>
+#   map(\(effect) effect * results$shock_cons_empleos) |>
+#   set_names(c("cons_directos", "cons_indirectos",
+#               "cons_desbordamiento", "cons_retroalimentacion"))
+# 
+# ops <- percents_effects |>
+#   map(\(effect) effect * results$shock_ops_year_empleos) |>
+#   set_names(c("ops_directos", "ops_indirectos",
+#               "ops_desbordamiento", "ops_retroalimentacion"))
+
+
+get_effects <- function(x) {
+  tmp <- percents_effects |>
+    map(\(effect) effect * x) |>
+  set_names(c("dir", "indir",
+              "desb", "retro"))
+  inducidos <- tmp$desb + tmp$retro
+  list(directos = tmp$dir,
+       indirectos = tmp$indir,
+       inducidos = inducidos)
+}
+
+
